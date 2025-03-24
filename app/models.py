aefc6243c9bdef6e729b4067ec04657f47672aad
gustavsonbarros <gustavson.adm@gmail.com>
2025-03-23 21:43:33 -0300

    Add validacao em tempo real

--- app/models.py
+++ app/models.py
@@ -1,9 +1,12 @@
 from app import db, login_manager
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,6 +15,13 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
 class AutisticPerson(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
@@ -19,4 +29,11 @@ class AutisticPerson(db.Model):
     father_name = db.Column(db.String(100), nullable=False)
     mother_name = db.Column(db.String(100), nullable=False)
     birth_date = db.Column(db.Date, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)  
     approved = db.Column(db.Boolean, default=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
